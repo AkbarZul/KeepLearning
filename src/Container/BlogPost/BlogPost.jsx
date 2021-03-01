af1ecbfc22bcc6da3ccd6dc2a44047f558be1a27
AkbarZul <akbar.zul73@gmail.com>
2021-03-01 21:16:02 +0700

    Consume API use axios

--- src/Container/BlogPost/BlogPost.jsx
+++ src/Container/BlogPost/BlogPost.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Post from "../../Component/Post/Post";
 import "./BlogPost.css";
+import axios from 'axios';
 
 class BlogPost extends Component {
   state = {
@@ -8,14 +9,29 @@ class BlogPost extends Component {
   };
 
   // read API use fetch
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/posts")
+//       .then((response) => response.json())
+//       .then((json) => {
+//           this.setState({
+//               post: json
+//           })
+//       });
+//   }
+
+
+  // read API use axios
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => {
-          this.setState({
-              post: json
-          })
-      });
+      axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        //   console.log(res.data)
+        this.setState({
+            post: res.data
+        })
+      })
+      .catch((err) => {
+          console.log(err)
+      })
   }
   render() {
     return (
@@ -23,7 +39,7 @@ class BlogPost extends Component {
         <p className="section-title">Blog post</p>
        {
            this.state.post.map(post => {
-               return <Post title={post.title} desc={post.body} />
+               return <Post key={post.id} title={post.title} desc={post.body} />
            })
        }
        
