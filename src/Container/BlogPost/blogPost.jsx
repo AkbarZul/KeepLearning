1c2c5a38b8fc9d629d7776e0c2e2c04c741f7553
AkbarZul <akbar.zul73@gmail.com>
2021-03-03 21:34:24 +0700

    React Hook read

---
+++ src/Container/BlogPost/blogPost.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import Post from "../../Component/Post/Post";
+import "./BlogPost.css";
+import axios from 'axios';
+
+const blogPost = () =>  {
+  const [post, setPost] = useState([])
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        const post = res.data
+        setPost(post)
+      })
+      .catch((err) => {
+          console.log(err)
+      })
+  }, [])
+    return (
+      <>
+        <p className="section-title">Blog post</p>
+       {
+           post.map(post => {
+               return <Post key={post.id} title={post.title} desc={post.body} />
+           })
+       }
+       
+      </>
+    );
+  }
+
+
+export default blogPost;
