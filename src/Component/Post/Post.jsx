7b28307cbdace816808555fba456691a0f0f5b03
AkbarZul <akbar.zul73@gmail.com>
2021-03-11 14:07:18 +0700

    Add json-serve

--- src/Component/Post/Post.jsx
+++ src/Component/Post/Post.jsx
@@ -1,16 +1,34 @@
-import React from "react";
-const urlPoster = 'http://image.tmdb.org/t/p/w500';
+// import React from "react";
+// const urlPoster = 'http://image.tmdb.org/t/p/w500';
 
-const Post = ({title, desc, img, overview}) => {
+// const Post = ({title, desc, img, overview}) => {
+//   return (
+//     <div className="post">
+//       <div className="img-thumb">
+//         <img src={urlPoster + img} alt="" />
+//       </div>
+//       <div className="content">
+//         <p className="title">{title}</p>
+//         <p className="desc">{desc}</p>
+//         <p className="desc">{overview}</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// Prawito
+import React from "react";
+
+const Post = ({title, desc}) => {
   return (
     <div className="post">
       <div className="img-thumb">
-        <img src={urlPoster + img} alt="" />
+        <img src='https://placeimg.com/640/480/tech' alt="" />
       </div>
       <div className="content">
         <p className="title">{title}</p>
         <p className="desc">{desc}</p>
-        <p className="desc">{overview}</p>
+        <button>Remove</button>
       </div>
     </div>
   );
--- src/Container/BlogPost/BlogPost.jsx
+++ src/Container/BlogPost/BlogPost.jsx
@@ -1,9 +1,68 @@
+// import React, { Component } from "react";
+// import Post from "../../Component/Post/Post";
+// import "./BlogPost.css";
+// import axios from "axios";
+// const url =
+//   "https://api.themoviedb.org/3/movie/top_rated?api_key=2246337a80611cc7c7330ca5156842f5&language=en-US&page=1";
+
+// class BlogPost extends Component {
+//   state = {
+//     post: [],
+//   };
+
+//   // read API use fetch
+//   //   componentDidMount() {
+//   //     fetch("https://jsonplaceholder.typicode.com/posts")
+//   //       .then((response) => response.json())
+//   //       .then((json) => {
+//   //           this.setState({
+//   //               post: json
+//   //           })
+//   //       });
+//   //   }
+
+//   // read API use axios
+//   componentDidMount() {
+//     axios
+//       .get(url)
+//       .then((res) => {
+//         console.log(res.data.results);
+//         this.setState({
+//           post: res.data.results,
+//         });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//       });
+//   }
+//   render() {
+//     const { post } = this.state;
+//     return (
+//       <>
+//         <p className="section-title">Blog post</p>
+//         {post.map(({ id, title, original_language, poster_path, overview }) => {
+//           return (
+//             <Post
+//               key={id}
+//               title={title}
+//               desc={original_language}
+//               img={poster_path}
+//               overview={overview}
+//             />
+//           );
+//         })}
+//       </>
+//     );
+//   }
+// }
+
+// export default BlogPost;
+
+// Prawito
 import React, { Component } from "react";
 import Post from "../../Component/Post/Post";
 import "./BlogPost.css";
 import axios from "axios";
-const url =
-  "https://api.themoviedb.org/3/movie/top_rated?api_key=2246337a80611cc7c7330ca5156842f5&language=en-US&page=1";
 
 class BlogPost extends Component {
   state = {
@@ -24,11 +83,11 @@ class BlogPost extends Component {
   // read API use axios
   componentDidMount() {
     axios
-      .get(url)
+      .get("http://localhost:3004/posts")
       .then((res) => {
-        console.log(res.data.results);
+        console.log(res.data);
         this.setState({
-          post: res.data.results,
+          post: res.data,
         });
       })
       .catch((err) => {
@@ -36,23 +95,17 @@ class BlogPost extends Component {
       });
   }
   render() {
-    const { post } = this.state;
-    return (
+    const {post} = this.state
+    return(
       <>
-        <p className="section-title">Blog post</p>
-        {post.map(({ id, title, original_language, poster_path, overview }) => {
-          return (
-            <Post
-              key={id}
-              title={title}
-              desc={original_language}
-              img={poster_path}
-              overview={overview}
-            />
-          );
-        })}
+      <p className="section-title">Blog post</p>
+      {
+        post.map((post => {
+          return <Post key={post.id} title={post.title} desc={post.body} />
+        }))
+      }
       </>
-    );
+    )
   }
 }
 
