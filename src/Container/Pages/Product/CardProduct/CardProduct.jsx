ac56f2ee44245b5ef6ced4e27eb7e636b9c81c6e
AkbarZul <akbar.zul73@gmail.com>
2021-03-12 20:29:20 +0700

    Props Counter different folder

--- src/Container/Pages/Product/CardProduct/CardProduct.jsx
+++ src/Container/Pages/Product/CardProduct/CardProduct.jsx
@@ -1,32 +1,7 @@
 import React, { Component } from "react";
+import Counter from './Counter';
 
 class CardProduct extends Component {
-    state = {
-        order: 0
-    }
-
-    handleCounterChange = (newValue) => {
-        this.props.onCounterChange(newValue);
-    }
-
-    handlePlus = () => {
-        this.setState({
-            order: this.state.order + 1
-        }, () => {
-            this.handleCounterChange(this.state.order);
-        })
-    }
-
-    handleMinus = () => {
-        if(this.state.order > 0) {
-            this.setState({
-                order: this.state.order - 1
-            }, () => {
-                this.handleCounterChange(this.state.order);
-            })
-        }
-    }
-
     render() {
         return (
             <>
@@ -36,11 +11,7 @@ class CardProduct extends Component {
                 </div>
                 <p className='product-title'>Daging Sapi Local</p>
                 <p className='product-price'>Rp. 50.000</p>
-                <div className='counter'>
-                    <button className='minus' onClick={this.handleMinus}>-</button>
-                    <input type="text" value={this.state.order} />
-                    <button className='plus' onClick={this.handlePlus}>+</button>
-                </div>
+               <Counter onCounterChange={(value) => this.props.onCounterChange(value)} />
             </div>
             </>
         )
